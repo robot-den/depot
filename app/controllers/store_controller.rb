f613b3378a9ef45b5eba5b6a147eefb0655989b4
robot-den <w.nazmutdinov@gmail.com>
2016-02-27 21:56:09 +0500

    all before D task

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @index_view_count = index_view_counter
     @products = Product.order(:title)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referented_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,7 +9,15 @@ class Product < ActiveRecord::Base
   validates :title, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)}i,
-    message: "Only GIF, JPG or PNG format!"
-  }
+    message: "Only GIF, JPG or PNG format!"}
 
+  private
+  def ensure_not_referented_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'it have line items')
+      false
+    end
+  end
 end
