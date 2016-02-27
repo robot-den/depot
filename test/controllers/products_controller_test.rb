69a3493334d96e18222983ce8c46e37a94605587
robot-den <w.nazmutdinov@gmail.com>
2016-02-27 16:16:31 +0500

    add store functional

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_actions a', minimum: 3
+    assert_select '#main .list_image', 3
+    assert_select 'h1', 'Listing Products'
   end
 
   test "should get new" do
